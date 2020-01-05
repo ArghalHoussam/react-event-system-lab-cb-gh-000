9eff8ff6d44bcb2b17cfe474bee784c68bb0eacb
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-05 19:54:57 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,7 +3,11 @@ import React from 'react';
 
 export default class Keypad extends React.Component {
   logSomeThing() {
+<<<<<<< HEAD
     console.log('Entering password...');
+=======
+    console.log('Entering password ...');
+>>>>>>> 50c6af458530a443b3a2d7ee2ad032e63885e6c6
   }
   render() {
     return (
